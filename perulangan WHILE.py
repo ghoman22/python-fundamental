a5bbe3594b050a613fdbec3a05f8248fc035459f
firman <firmanap22@gmail.com>
2021-11-29 21:35:56 +0700

    perulangan dengan for dan while untuk jumlah buku tertentu

---
+++ perulangan WHILE.py
@@ -0,0 +1,16 @@
+"""
+program perulangan dengan WHILE
+"""
+
+jumlah_buku =100
+print('Perintah ibu "Baca semua bukumu"')
+
+jumlah_buku_yang_dibaca = 0
+print(f"jumlah buku yang sudah dibaca {jumlah_buku_yang_dibaca}")
+
+while jumlah_buku_yang_dibaca < jumlah_buku:
+    print(f"Buku ke {jumlah_buku_yang_dibaca + 1} sudah dibaca")
+    jumlah_buku_yang_dibaca = jumlah_buku_yang_dibaca + 1
+
+print(f"jumlah buku yang sudah dibaca {jumlah_buku_yang_dibaca}")
+
